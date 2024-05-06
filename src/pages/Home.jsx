2887a70af739d5122d3dd97176e6c17d9cec1f2c
Emmanuel-Wambua <mantelmanu31@gmail.com>
2024-05-06 19:35:59 +0300

    Changed motivation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <div>ACHIEVE</div>
       </div>
       <div className=" ml-96 text-3xl pl-10 pt-10">
-         <p className="text-white ">Stay Fucking Hard Always...David Goggins</p>
+         <p className="text-white ">Push Harder Than Yesterday...</p>
       </div>
       <div className="ml-96 pl-10 pt-10">
       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign-Up</button>
