823d5bd23bb0758eed7c2a2da3255d3abe3d674d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2024-05-06 10:35:19 +0300

    install new dependencies and clean data

--- src/pages/display.jsx
+++ src/pages/display.jsx
@@ -3,7 +3,7 @@ export default function display(){
         <>
         <div>
             <div >
-                <div class="h-2000 bg-gradient-to-r from-purple-500 to-pink-500 pb-96">
+                <div className="h-2000 bg-gradient-to-r from-purple-500 to-pink-500 pb-96">
                     <p  className="pb-80">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor vol uptates incidunt vitae quas repudiandae quidem soluta natus voluptatem omnis ipsa, ab atque earum quis odio eveniet esse dolorum aut corporis?</p>
                     <p className="text-3xl p-100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque exercitationem aliquam iusto! Tempore facilis, cumque nisi vel dolores eius assumenda quis. Repudiandae aspernatur, dolor obcaecati possimus quo voluptatum beatae error.</p>
                     <p className="mb-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit minus iste tempora ab nihil dolor laborum, repellendus perferendis explicabo ducimus molestiae perspiciatis aliquam vel aut velit dignissimos reprehenderit iusto!</p>
